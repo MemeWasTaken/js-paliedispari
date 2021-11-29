7cef44bb2bbf66e00f266ce2bf9013b52e3da5e9
MemeWasTaken <memelazzari02@gmail.com>
2021-11-29 15:31:44 +0100

    Pari e Dispari: v.1

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -4,3 +4,20 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari.
 // Dichiariamo chi ha vinto (corrispondenza tra scelta pari e dispari e somma pari o dispari).
 // In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari.
+
+let playerGuess = parseInt(prompt('Inserisci "0" se preferisci pari o "1" se preferisci dispari'));
+let playerNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+console.log(playerGuess);
+console.log(playerNumber);
+let aiNumber =  Math.floor(Math.random() * (5 - 1) ) + 1;
+console.log(aiNumber);
+let sum = playerNumber + aiNumber;
+console.log(sum);
+
+if (playerGuess == (sum % 2) ) {
+    console.log('HAI VINTO');
+    alert('Complimenti hai battuto il computer');
+} else {
+    console.log('Hai perso');
+    alert('Hai perso, peccato');
+}
