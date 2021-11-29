ae8d1fedb713c8d7652ea9bc8511f5649f646f07
MemeWasTaken <memelazzari02@gmail.com>
2021-11-29 15:19:08 +0100

    Palindroma: v.2

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -2,18 +2,31 @@
 // Creare una funzione per capire se la parola inserita è palindroma (prima senza funzione e poi con funzione).
 
 let wordUser = prompt('Inserisci una parola da controllare:').toLowerCase();
-// console.log(wordUser.length);
-// console.log(wordUser);
-const charsWordUser = [];
-// console.log(charsWordUser);
-for (let i = 0; i < wordUser.length; i++) {
-    console.log(i);
-    let charWordUser = wordUser.charAt(i);
-    charsWordUser.unshift(charWordUser);
+//  console.log(wordUser.length);
+//  console.log(wordUser);
+// const charsWordUser = [];
+//  console.log(charsWordUser);
+// for (let i = 0; i < wordUser.length; i++) {
+//     console.log(i);
+//     let charWordUser = wordUser.charAt(i);
+//     charsWordUser.unshift(charWordUser);
+// }
+// let reverseWord = charsWordUser.toString().replace(/,/g,'');
+// console.log(reverseWord);
+// if (reverseWord === wordUser) {
+//     console.log('PALINDROMA');
+//     alert('La parola inserita è palindroma');
+// } else {
+//     console.log('Non è palindroma');
+//     alert('La parola inserita non è palindroma');
+// }
+
+function reverseString() {
+    return wordUser.split("").reverse().join("");
 }
-let reverseWord = charsWordUser.toString().replace(/,/g,'');
-console.log(reverseWord);
-if (reverseWord === wordUser) {
+
+let reversedWord = reverseString();
+if (reversedWord === wordUser) {
     console.log('PALINDROMA');
     alert('La parola inserita è palindroma');
 } else {
