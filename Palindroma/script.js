20443d4ee1a79f18350a7e22c1c10e1beb598191
MemeWasTaken <memelazzari02@gmail.com>
2021-11-29 15:08:03 +0100

    Palindroma: v.1

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,2 +1,22 @@
 // Chiedere all’utente di inserire una parola.
 // Creare una funzione per capire se la parola inserita è palindroma (prima senza funzione e poi con funzione).
+
+let wordUser = prompt('Inserisci una parola da controllare:').toLowerCase();
+// console.log(wordUser.length);
+// console.log(wordUser);
+const charsWordUser = [];
+// console.log(charsWordUser);
+for (let i = 0; i < wordUser.length; i++) {
+    console.log(i);
+    let charWordUser = wordUser.charAt(i);
+    charsWordUser.unshift(charWordUser);
+}
+let reverseWord = charsWordUser.toString().replace(/,/g,'');
+console.log(reverseWord);
+if (reverseWord === wordUser) {
+    console.log('PALINDROMA');
+    alert('La parola inserita è palindroma');
+} else {
+    console.log('Non è palindroma');
+    alert('La parola inserita non è palindroma');
+}
